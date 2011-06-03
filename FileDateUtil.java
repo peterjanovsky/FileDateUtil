b811d7c28865213246575fef31971861701b10a6
peter janovsky <peterjanovsky@gmail.com>
2011-06-03 12:56:15 -0700

    updated error handling

--- FileDateUtil.java
+++ FileDateUtil.java
@@ -20,8 +20,7 @@ public class FileDateUtil {
 			
 			dt = dateFormat.parse(date);
 		} catch (ParseException e) {
-			String stackTrace = ExceptionUtil.convertStackTrace(e);
-			MailUtil.getInstance().sendMessage(e.getMessage() + " - " + stackTrace);
+			// do something
 		}
 		
 		return dt;
